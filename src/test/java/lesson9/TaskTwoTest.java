75e1e4602a9f5c6a2f4db9dbf2846958797f9b8b
munirov <neglavmag@gmail.com>
2021-06-19 21:49:36 +0300

    Lesson 9 Task 2

---
+++ src/test/java/lesson9/TaskTwoTest.java
@@ -0,0 +1,43 @@
+package lesson9;
+
+import lib.CoreTestCase;
+import lib.Platform;
+import lib.ui.SearchPageObject;
+import lib.ui.factories.SearchPageObjectFactory;
+import org.junit.Test;
+
+import java.util.List;
+
+public class TaskTwoTest extends CoreTestCase {
+
+    // Адаптировать по MW тест на поиск и верификацию трех результатов выдачи поиска.
+    @Test
+    public void testSearchByTitleAndDescription() {
+        String searchTitle = "Pink Floyd album";
+        String searchDescription = "album by Pink Floyd";
+
+        String mwSearchTitle = "Pink Floyd";
+        String mwSearchDescription = "English rock band";
+
+        SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
+
+        if (Platform.getInstance().isAndroid()) {
+            SearchPageObject.skipSettings();
+            SearchPageObject.initSearchInput();
+            SearchPageObject.typeSearchLine(searchTitle);
+            SearchPageObject.waitForElementByTitleAndDescription(searchTitle, searchDescription);
+        }
+
+        if (Platform.getInstance().isMw()) {
+            SearchPageObject.initSearchInput();
+            SearchPageObject.typeSearchLine(mwSearchTitle);
+            SearchPageObject.waitForElementByTitleAndDescription(mwSearchTitle, mwSearchDescription);
+        }
+
+
+
+        List<String> articlesTitles = SearchPageObject.getArticleListBySearch();
+
+        assertTrue(articlesTitles.size() >= 3);
+    }
+}
